9451c6663a67f2bd3f40e224d71f9bcbfda0a3c6
Antonette <amgichohu@gmail.com>
2020-02-08 16:13:30 -0600

    more google api stuff

--- grocery-hunter/src/App.js
+++ grocery-hunter/src/App.js
@@ -3,12 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 import GMap from './GoogleMapsApi/maps';
 
+// var integer = gmap.adder(5,4)
+
+
 function App() {
+  
   return (
     <div className="App">
       <header className="App-header">
         <div style={{height: '50vh', width:'50%'}}>
-          <GMap />
+          <GMap/>
+          <div id="map"></div>
         </div>
       </header>
       
