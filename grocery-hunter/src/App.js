dacbd0153c0a5a6937e5e5f5417df59bb239bfc4
Antonette <amgichohu@gmail.com>
2020-02-08 13:34:36 -0600

    google maps api test added

--- grocery-hunter/src/App.js
+++ grocery-hunter/src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import GMap from './GoogleMapsApi/maps';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div style={{height: '50vh', width:'50%'}}>
+          <GMap />
+        </div>
       </header>
+      
     </div>
   );
 }
